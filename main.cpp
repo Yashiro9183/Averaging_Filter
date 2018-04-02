cd4f6f034f5a38542b29ad6ac7553677f8aa99c8
Yashiro9183 <tat.yashiro@gmail.com>
2018-04-02 09:11:58 +0900

    second commit

--- main.cpp
+++ main.cpp
@@ -13,28 +13,33 @@ int main(int argc,char* argv[]) {
 	std::cout << "フィルタサイズ入力:k=" << std::endl;
 	std::cin >> k;
 
-	
+	cv::Mat dst_img = cv::Mat::zeros(cv::Size(src_img.rows,src_img.cols),CV_8UC3);
+
 	for (int BGR = 0; BGR < 3; BGR++) {
-		for (int x = (k-1)/2; x < src_img.rows-(k-1)/2; x++) {
-			for (int y = (k-1)/2; y < src_img.cols-(k-1)/2; y++) {
+		for (int x = 0; x < src_img.rows; x++) {
+			for (int y = 0; y < src_img.cols; y++) {
 				int tem = 0;
+				int num = 0;
 				for (int i = -(k-1)/2; i < (k-1)/2; i++) {
 					for (int j = -(k-1)/2; j < (k-1)/2; j++) {
-						if ((y + i < 0) || (x + j < 0)) {
-							src_img.at<cv::Vec3b>(y + i, x + j)[BGR] = 0;
+						if ((y + i < 0) || (x + j < 0)||(src_img.rows<y+i)||(src_img.cols<x+j)) break;
+						else {
+							tem += src_img.at<cv::Vec3b>(y + i, x + j)[BGR];
+							num++;
 						}
-						tem += src_img.at<cv::Vec3b>(y + i, x + j)[BGR];
+
 					}
 				}
-				tem = tem / (k*k);
+				tem = tem / num;
 				if (tem > 255)tem = 255;
-				src_img.at<cv::Vec3b>(y, x)[BGR] = tem;
+				dst_img.at<cv::Vec3b>(y, x)[BGR] = tem;
 			}
 		}
 	}
 
-		cv::imshow("Averaging_Filter", src_img);
-		cv::imwrite("AveragingFilter.jpg", src_img);
+		cv::imshow("Averaging_Filter", dst_img);
+		cv::imshow("View", src_img);
+		cv::imwrite("AveragingFilter.jpg", dst_img);
 		cv::waitKey();
 		system("pause");
 		
